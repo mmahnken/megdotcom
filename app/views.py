07b153938e896b9e7aa1e21431fc37b152960256
Meggie Mahnken <mmm25eg@gmail.com>
2013-05-10 14:58:05 -0700

    added projects.

--- app/views.py
+++ app/views.py
@@ -5,3 +5,12 @@ from app import app
 @app.route('/index')
 def index():
 	return render_template("index.html")
+
+@app.route('/blog')
+def blog():
+	return render_template("blog.html")
+
+@app.route('/projects')
+def projects():
+	return render_template("projects.html")
+	
